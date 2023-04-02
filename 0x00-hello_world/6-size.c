92ab3358bea99c6c995c204543c7083cc3594644
Karimaame <amezgou@gmail.com>
2023-04-02 15:21:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point 
+*
+* Return: 0(success)
+*/
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+        printf("with proper grammar, but the outcome is a piece of art\n,");
+
+return (0);
+}
